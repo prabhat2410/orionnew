742a4d2437a95a1587bf83cbef88d6f3696096dc
prabhat2410 <40513512+prabhat2410@users.noreply.github.com>
2019-11-25 16:36:17 +0530

    Update contextBroker.cpp

--- src/app/contextBroker/contextBroker.cpp
+++ src/app/contextBroker/contextBroker.cpp
@@ -235,6 +235,8 @@ bool            ngsiv1Autocast;
 #define INSECURE_NOTIF         "allow HTTPS notifications to peers which certificate cannot be authenticated with known CA certificates"
 #define NGSIV1_AUTOCAST        "automatic cast for number, booleans and dates in NGSIv1 update/create attribute operations"
 
+//testing file ****prabhat****
+test1=
 
 
 /* ****************************************************************************
